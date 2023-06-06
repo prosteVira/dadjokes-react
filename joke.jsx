a354800f13de7899cfa4a99474a33327db32ebb3
Vira <126858912+prosteVira@users.noreply.github.com>
2023-06-06 14:23:00 +0300

    j

---
+++ joke.jsx
@@ -0,0 +1,20 @@
+export const Joke = ({ userAvatar, userName, text, likes, dislikes, key }) => (
+ <div className="container" key={key}>
+  <div className="joke">
+    <div className="joke__body">
+      <div className="joke__user">
+        <img className="user-avatar" src={userAvatar} />
+        <p className="user-name">{userName}</p>
+      </div>
+
+      <p className="joke__text">{text}</p>
+    </div>
+    <div className="joke__likes">
+      <button id="btn-up" className="btn-like btn-like--up"></button>
+      <span id="likes-up" className="likes-count likes-count--up">{likes}</span>
+      <button id="btn-down" className="btn-like btn-like--down"></button>
+      <span id="likes-down" className="likes-count likes-count--down">{dislikes}</span>
+    </div>
+  </div>
+ </div>
+ );
